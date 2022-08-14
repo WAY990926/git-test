b50f4c81e39fa15ae2c7f3ea147e4a0402e9f581
ws <ws10@dareway.com.cn>
2022-08-14 21:55:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author ws
+ * @date 2022/8/14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!!!");
+    }
+}
